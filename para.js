3517c7debf9eb27e715b13e73e71f24cdb5c8c9e
Masum267 <abdullahalmasum267@gmail.com>
2023-06-08 20:58:40 +0600

    All file of typespeed game

---
+++ para.js
@@ -0,0 +1,5 @@
+const paragraphs = [
+    "Because JSON syntax is derived from JavaScript object notation, very little extra software is needed to work with JSON within JavaScript.With JavaScript you can create an array of objects and assign data to it, like this:",
+    "In the example above, the object 'employees' is an array containing three objects. Each object is a record of a person (with a first name and a last name).",
+    "It is safer to use a JSON parser to convert a JSON text to a JavaScript object.A JSON parser will recognize only JSON text and will not compile scripts.In browsers that provide native JSON support, JSON parsers are also faster.Native JSON support is included in all major browsers and in the latest ECMAScript (JavaScript) standard:"
+]
